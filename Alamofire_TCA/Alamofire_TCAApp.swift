d6a5539abd10a6253cedd89800e1df1fb77b04ba
EJLee1209 <dldmswo1209@gmail.com>
2023-04-30 15:05:43 +0900

    Initial Commit

---
+++ Alamofire_TCA/Alamofire_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alamofire_TCAApp.swift
+//  Alamofire_TCA
+//
+//  Created by 이은재 on 2023/04/30.
+//
+
+import SwiftUI
+
+@main
+struct Alamofire_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
